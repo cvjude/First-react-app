9ad71b98841a8ae5265ecba610186e326e9a66d3
Jude Chinoso <judechinoso@Judes-MacBook-Pro.local>
2019-07-04 10:17:18 +0100

    Testing out react frame work with web pack

---
+++ index.js
@@ -0,0 +1,11 @@
+import react from "react";
+import reactDom from "react-dom";
+
+reactDom.render(
+  <ul>
+    <li>jude</li>
+    <li>violet</li>
+    <li>chinoso</li>
+  </ul>,
+  document.getElementById("root")
+);
