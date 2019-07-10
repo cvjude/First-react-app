ec82701636d17a5c453a57a55be569418ba68afe
cvjude <jjchinosoviolet@gmail.com>
2019-07-10 14:22:26 +0100

    handle events with react

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div>
-      <img src="https://www.fillmurray.com/200/100" />
-      <br />
-      <br />
-      <button
-        onClick={() => {
-          console.log("I was clicked");
-        }}
-      >
-        Click me
-      </button>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => {
+      return {
+        count: prevState.count + 1
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.handleClick}>Change!</button>
+      </div>
+    );
+  }
 }
 
 export default App;
