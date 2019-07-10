5b1d5230e0f53d5334306f457992e851be7b2358
cvjude <jjchinosoviolet@gmail.com>
2019-07-10 17:12:15 +0100

    Conditional rendering on log in and out

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,33 @@
 import React, { Component } from "react";
-import TodoItem from "./TodoItem";
-import todoData from "../models/todoData";
-import style from "../styles/style.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      todos: todoData
+      isLoggedIn: false
     };
-    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  handleChange(id) {
+  handleClick() {
     this.setState(prevState => {
-      const updatedTodos = prevState.todos.map(todo => {
-        if (todo.id === id) {
-          todo.completed = !todo.completed;
-        }
-        return todo;
-      });
       return {
-        todos: updatedTodos
+        isLoggedIn: !prevState.isLoggedIn
       };
     });
   }
 
   render() {
-    const { todos } = this.state;
-    const todoComponents = todos.map(todoItem => (
-      <TodoItem
-        key={todoItem.id}
-        todo={todoItem}
-        handleChange={this.handleChange}
-      />
-    ));
-
-    return <div className="todo-list">{todoComponents}</div>;
+    console.log(this.state.isLoggedIn);
+    const isLog = this.state.isLoggedIn
+      ? ["Logout", "Logged out"]
+      : ["Login", "Logged In"];
+    return (
+      <div>
+        <h2>{isLog[1]}</h2>
+        <button onClick={this.handleClick}>{isLog[0]}</button>
+      </div>
+    );
   }
 }
 
