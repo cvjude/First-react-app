4f5fa758aa29a4c1757197955963cb1797692a2f
cvjude <jjchinosoviolet@gmail.com>
2019-07-13 11:15:57 +0100

    Finished the form

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Form from "./Form.js";
 import style from "../style.css";
 
 class App extends Component {
@@ -7,9 +8,12 @@ class App extends Component {
     this.state = {
       firstName: "",
       lastName: "",
-      isFriendly: false,
+      age: "",
       gender: "",
-      favColor: ""
+      location: "",
+      Vegetarian: true,
+      Kosher: true,
+      LactoseFree: true
     };
     this.handleChange = this.handleChange.bind(this);
   }
@@ -23,63 +27,9 @@ class App extends Component {
 
   render() {
     return (
-      <form>
-        <input
-          name="firstName"
-          value={this.state.firstName}
-          type="text"
-          placeholder="FirstName"
-          onChange={this.handleChange}
-        />
-        <input
-          name="lastName"
-          value={this.state.lastName}
-          type="text"
-          placeholder="LastName"
-          onChange={this.handleChange}
-        />
-        <input
-          name="isFriendly"
-          id="isFriendly"
-          type="checkbox"
-          checked={this.state.isFriendly}
-          onChange={this.handleChange}
-        />
-        <label htmlFor="isFriendly">click</label>
-        <label>
-          <input
-            name="gender"
-            type="radio"
-            value="male"
-            checked={this.state.gender === "male"}
-            onChange={this.handleChange}
-          />
-          Male
-        </label>
-        <label>
-          <input
-            name="gender"
-            type="radio"
-            value="female"
-            checked={this.state.gender === "female"}
-            onChange={this.handleChange}
-          />
-          Female
-        </label>
-
-        <select
-          value={this.state.favColor}
-          onChange={this.handleChange}
-          name="favColor "
-        >
-          <option value="red">Red</option>
-          <option value="blue">Blue</option>
-          <option value="green">Green</option>
-        </select>
-        <h1>
-          {this.state.firstName} {this.state.lastName}
-        </h1>
-      </form>
+      <main>
+        <Form {...this.state} handleChange={this.handleChange} />
+      </main>
     );
   }
 }
